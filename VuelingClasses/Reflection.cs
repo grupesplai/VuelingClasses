2ac996b96a180e59d44cbcfdddbc57c232179da7
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-06-28 18:33:57 +0200

    primer commit with comments. close #1

---
+++ VuelingClasses/Reflection.cs
@@ -0,0 +1,15 @@
+﻿namespace VuelingClasses
+{
+    class Customer
+    {
+        public Customer() {   }
+        public Customer(string name)
+        {
+            Name = name;
+        }
+        public int Id {  get; set;  }
+
+        public string Name{ get; set;}
+        
+    }
+}
