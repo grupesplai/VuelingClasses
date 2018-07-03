14d41e48ca415e05f49031b60aafd04c87fefcf5
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-03 08:52:33 +0200

    Modificación archivo con reflection. issue #5

--- VuelingClasses/Reflection/ReflectionXmlXpathLinq.cs
+++ VuelingClasses/Reflection/ReflectionXmlXpathLinq.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -20,6 +21,7 @@ namespace VuelingClasses.Reflection
             foreach (XmlNode node in nodeList)
                 customerList.Add(node["Customer"].InnerText);
             Console.WriteLine("To read Xml file with Xpath");
+            
             foreach (string cl in customerList)
                 Console.WriteLine(cl);
 
@@ -30,8 +32,24 @@ namespace VuelingClasses.Reflection
             Console.WriteLine("\nTo read Xml file using LinqToXml");
             foreach (var re in result)
                 Console.WriteLine(re);
+
+            Assembly assembly = typeof(ReflectionXmlXpath).Assembly;
+            Type customerType = assembly.GetType("VuelingClasses.Reflection.Customer");
+            Customer customerObject = (Customer)Activator.CreateInstance(customerType, customerList[0]);
+            //ahora los datos parametrizados estan en la ram
+            Console.WriteLine("The customer object in ram is: " + (customerObject).Name);
+
         }
+    }
+    class Customer
+    {
+        public Customer() { }
+        public Customer(string name)
+        {
+            Name = name;
+        }
+        //public int Id {  get; set;  }
+        public string Name { get; set; }
 
     }
-
 }
