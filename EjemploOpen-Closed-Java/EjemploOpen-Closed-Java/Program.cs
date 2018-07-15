a00aabaef0a1c0d16adfbba31f5fdfa2124df004
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-15 09:40:51 +0200

    Ejemplo Open-Closed desde java

---
+++ EjemploOpen-Closed-Java/EjemploOpen-Closed-Java/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploOpen_Closed_Java
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double Area()
+        {
+            return Radius * Radius * Math.PI;
+        }
+    }
+
+}
