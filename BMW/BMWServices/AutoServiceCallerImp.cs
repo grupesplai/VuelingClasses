900c03b8b803207c5f80ffe70543514cbab629e0
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-24 16:57:03 +0200

    Injection Dependency exemple from the constructor

---
+++ BMW/BMWServices/AutoServiceCallerImp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMW
+{
+    public class AutoServiceCallerImp : IAutoServiceCaller
+    {
+        private BMWAutoService bmwAutoService;
+        private FordAutoService hondaAutoService;
+        private HondaAutoService fordAutoService;
+
+        public AutoServiceCallerImp(BMWAutoService bmwAutoService, FordAutoService hondaAutoService, HondaAutoService fordAutoService)
+        {
+            this.bmwAutoService = bmwAutoService;
+            this.fordAutoService = fordAutoService;
+            this.hondaAutoService = hondaAutoService;
+        }
+        
+        public void callAutoService()
+            {
+                bmwAutoService.getService();
+                fordAutoService.getService();
+                hondaAutoService.getService();
+            }
+    }
+}
